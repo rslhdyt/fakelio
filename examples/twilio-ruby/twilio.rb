ab5417578f2e26dd35a999bb8080c94cba3cbddb
Risal Hidayat <risal.hidayat@kawanlamacorp.com>
2025-08-17 20:26:57 +0700

    Add Twilio Ruby example with Gemfile, Gemfile.lock, and initial Twilio client implementation

---
+++ examples/twilio-ruby/twilio.rb
@@ -0,0 +1,40 @@
+require 'twilio-ruby'
+
+# monkey patch to use fakelio.rslhdyt.dev as the base url
+module Twilio
+  module REST
+    class TrusthubBase < Domain
+      ##
+      # Initialize trusthub domain
+      #
+      # @param twilio - The twilio client
+      #
+      def initialize(twilio)
+        super(twilio)
+        @base_url = "https://fakelio.rslhdyt.dev"
+        @host = "fakelio.rslhdyt.dev"
+        @port = 443
+        @v1 = nil
+      end
+
+      def v1
+        @v1 ||= Trusthub::V1.new self
+      end
+
+      ##
+      # Provide a user friendly representation
+      def to_s
+        '<Twilio::REST::Trusthub::V1>';
+      end
+    end
+  end
+end
+
+client = Twilio::REST::Client.new(
+  "BANANA",
+  "BANANA",
+)
+
+response = client.trusthub.v1.customer_profiles.list
+
+puts response.inspect
